1ac38d24b3acaffed9a91170cba77e729065a5f2
Théo Seigneuret-Poussard <theo.seigneuret-poussard@scantech.com>
2024-07-15 15:45:23 +0200

    First version of packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='TennisRPG',
+	version='0.1',
+	packages=find_packages(),
+	install_requires=[
+		'Faker>=25.8.0',
+		'transliterate>=1.10.2',
+		'unidecode~=1.3.8'
+	],
+	entry_points={
+		'console_scripts': [
+			"TennisRPG = TennisRPG.main:main"
+		]
+	}
+)
