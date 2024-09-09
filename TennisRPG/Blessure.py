a12cdb7b93a1b2eb9328213440e9e5b2789f679c
Théo Seigneuret-Poussard <theo.seigneuret-poussard@scantech.com>
2024-09-09 15:54:21 +0200

    Fix and stabilisation

--- TennisRPG/Blessure.py
+++ TennisRPG/Blessure.py
@@ -46,36 +46,35 @@ dico_blessures = {
 		{"nom": "Coup de chaleur", "gravite": 1, "repos": 1}
 	],
 	2: [
-		{"nom": "Contracture musculaire", "gravite": 2, "repos": 1},
-		{"nom": "Élongation du mollet", "gravite": 2, "repos": 1},
-		{"nom": "Syndrome du canal carpien", "gravite": 2, "repos": 2},
-		{"nom": "Entorse du poignet", "gravite": 2, "repos": 2},
+		{"nom": "Contracture musculaire", "gravite": 2, "repos": 2},
+		{"nom": "Élongation du mollet", "gravite": 2, "repos": 2},
+		{"nom": "Syndrome du canal carpien", "gravite": 2, "repos": 3},
+		{"nom": "Entorse du poignet", "gravite": 2, "repos": 3},
 	],
 	3: [
-		{"nom": "Lombalgie", "gravite": 3, "repos": 2},
-		{"nom": "Syndrome de la bandelette ilio-tibiale", "gravite": 3, "repos": 3},
-		{"nom": "Tendinite légère", "gravite": 3, "repos": 3}
+		{"nom": "Lombalgie", "gravite": 3, "repos": 4},
+		{"nom": "Syndrome de la bandelette ilio-tibiale", "gravite": 3, "repos": 5},
+		{"nom": "Tendinite légère", "gravite": 3, "repos": 5}
 	],
 	4: [
-		{"nom": "Entorse de la cheville", "gravite": 4, "repos": 3},
-		{"nom": "Élongation musculaire", "gravite": 4, "repos": 3},
-		{"nom": "Tendinite de l'épaule", "gravite": 4, "repos": 4},
-		{"nom": "Déchirement partiel du ménisque", "gravite": 4, "repos": 5},
+		{"nom": "Entorse de la cheville", "gravite": 4, "repos": 4},
+		{"nom": "Élongation musculaire", "gravite": 4, "repos": 4},
+		{"nom": "Tendinite de l'épaule", "gravite": 4, "repos": 5},
+		{"nom": "Déchirement partiel du ménisque", "gravite": 4, "repos": 6},
 	],
 	5: [
-		{"nom": "Déchirure musculaire", "gravite": 5, "repos": 6},
-		{"nom": "Fracture simple", "gravite": 5, "repos": 6},
-		{"nom": "Épicondylite latérale", "gravite": 5, "repos": 8}
+		{"nom": "Déchirure musculaire", "gravite": 5, "repos": 7},
+		{"nom": "Fracture simple", "gravite": 5, "repos": 7},
+		{"nom": "Épicondylite latérale", "gravite": 5, "repos": 9}
 	],
 	6: [
-		{"nom": "Déchirure de la coiffe des rotateurs", "gravite": 6, "repos": 10},
-		{"nom": "Fracture osseuse complexe", "gravite": 6, "repos": 12},
-		{"nom": "Luxation de l'épaule", "gravite": 6, "repos": 14},
-		{"nom": "Déchirure ligamentaire", "gravite": 6, "repos": 16},
+		{"nom": "Déchirure de la coiffe des rotateurs", "gravite": 6, "repos": 12},
+		{"nom": "Fracture osseuse complexe", "gravite": 6, "repos": 13},
+		{"nom": "Luxation de l'épaule", "gravite": 6, "repos": 16},
 	],
 	7: [
-		{"nom": "Hernie discale", "gravite": 7, "repos": 14},
-		{"nom": "Rupture du tendon d'Achille", "gravite": 7, "repos": 20},
+		{"nom": "Déchirure ligamentaire", "gravite": 6, "repos": 20},
+		{"nom": "Rupture du tendon d'Achille", "gravite": 7, "repos": 22},
 		{"nom": "Rupture du ligament croisé antérieur", "gravite": 7, "repos": 36}
 	]
 }
